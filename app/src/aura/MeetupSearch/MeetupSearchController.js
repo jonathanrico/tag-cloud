d12fc32d16ca18d821a35388b3121d00371510fa
Jonathan Rico <jonathan.rm@gmail.com>
2015-05-04 23:55:35 -0700

    updated stuff

--- app/src/aura/MeetupSearch/MeetupSearchController.js
+++ app/src/aura/MeetupSearch/MeetupSearchController.js
@@ -1,6 +1,6 @@
 ({
     doSearch : function(component, event, helper) {
-        var meetupSearchEvent = $A.get("e.LightningMeetup:MeetupSearchEvent");
+        var meetupSearchEvent = $A.get("e.lmeetup:MeetupSearchEvent");
         var searchTerms = component.find('meetupSearch').get('v.value');
         var zipCode = component.find('searchZipCode').get('v.value');
         var radius = component.find('searchRadius').get('v.value');
